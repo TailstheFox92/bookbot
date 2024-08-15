dac7a7ac14d9ea29a8fd3adc5eb55e2eb6c4d398
Gabriel Fernandez <gfernandez@mailfence.com>
2024-08-15 13:00:37 -0400

    Added arguments, book reading, and word counting

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("hello world")
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("book_filepath")
+args = parser.parse_args()
+
+
+def read_book():
+    with open(args.book_filepath) as f:
+        file_contents = f.read()
+        return file_contents
+
+
+def count_words(words):
+    count = 0
+    for word in words:
+        count += 1
+    return count
+
+
+def main():
+    try:
+        words = read_book().split()
+        print(f"There are {count_words(words)} words in this book!")
+
+    except FileNotFoundError:
+        print("Could not find the specified file")
+
+
+main()
